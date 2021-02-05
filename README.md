5c3af09d58dbd2407fd080c7953a671084d6dd9b
Arno736 <52139210+Arno736@users.noreply.github.com>
2021-02-06 00:40:14 +0100

    v1.7

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tera Cross Server Tooltip
+
+Show tooltips when inspecting gear on players from other servers
+
+
+
+###### Notes :
+
+&curren; Etchings are not display
+
+&curren; 64bit make that some other servers player are considered playing on the same server
+
+
+
+
+
+
+
+This mod was an update of an older mod '[cross-server-tooltip](https://github.com/teralove/cross-server-tooltip)'
---
+++ index.js
@@ -0,0 +1,49 @@
+module.exports = function CrossServerTooltip(mod) {    
+    let paperdoll;
+    mod.hook('S_USER_PAPERDOLL_INFO', 11, (event) => { paperdoll = event; });
+    mod.hook('C_SHOW_ITEM_TOOLTIP_EX', 3, (event) => {
+        // 64bit make that some other servers player are considered playing on the same server
+        // if (event.serverId == 0) return; // Same sever detected (64bit bug : other server player = 0)
+
+        if (paperdoll == undefined) return; // Paperdoll is not defined
+
+        let item;
+        for(let i = 0; i < paperdoll.items.length; i++) {
+            if (paperdoll.items[i].dbid == event.id) {
+                item = paperdoll.items[i];
+                break;
+            }
+        }
+
+        if (item == undefined) return; // Abort if no item found
+
+        // Add cristals on earrings and rings
+        let crystals = [];
+        if ([6, 7, 8, 9].includes(item.slot)) {
+            if (item.crystal1 != 0) crystals.push(item.crystal1);
+        }
+        
+        // Show ToolTip
+        mod.send('S_SHOW_ITEM_TOOLTIP', 14, {
+            type: 24,
+            id: item.id,
+            dbid: item.dbid,
+            dbid2: item.dbid,
+            ownerId: item.playerId,
+            container: item.container,
+            slot: item.slot,
+            amount: item.amount,
+            enchant: 0,
+            soulbound: item.soulbound,
+            compareStats: false,
+            passivitySets: item.passivitySets,
+            soulboundName: paperdoll.name,
+            passivitySet: item.passivitySet,
+            passivitySets: item.passivitySets,
+            extraPassivitySets: item.extraPassivitySets,
+            mergedPassivities: item.mergedPassivities,
+            crystals: crystals,
+            secRemaining: -2
+        });
+    });
+}
